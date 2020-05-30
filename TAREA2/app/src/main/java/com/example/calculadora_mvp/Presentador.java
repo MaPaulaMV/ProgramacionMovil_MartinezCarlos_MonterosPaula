29a866ac8ff96dcff6bceb6e5b49ccc6d88a879a
MaPaulaMV <31556567+MaPaulaMV@users.noreply.github.com>
2020-05-30 15:02:27 -0500

    vista y presentador

---
+++ TAREA2/app/src/main/java/com/example/calculadora_mvp/Presentador.java
@@ -0,0 +1,28 @@
+package com.example.calculadora_mvp;
+
+public class Presentador implements iCalculadora.iPresentador{
+    private  iCalculadora.iVista iVista;
+    private iCalculadora.iModelo iModelo;
+
+    public Presentador(iCalculadora.iVista iVista){
+        this.iVista = iVista;
+        iModelo= new Modelo(this);
+    }
+
+    @Override
+    public void mostrarRespP(String resp) {
+        if (iVista != null){
+            iVista.mostrarRespV(resp);
+        }
+
+    }
+
+    @Override
+    public void calcularP(String num1, String num2,String operacion) {
+        if (iVista != null){
+            iModelo.calcularM(num1,num2,operacion);
+        }
+
+    }
+
+}
--- TAREA2/app/src/main/java/com/example/calculadora_mvp/Vista.java
+++ TAREA2/app/src/main/java/com/example/calculadora_mvp/Vista.java
@@ -3,12 +3,35 @@ package com.example.calculadora_mvp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Vista extends AppCompatActivity {
+public class Vista extends AppCompatActivity implements iCalculadora.iVista{
+
+    private EditText num1;
+    private EditText num2;
+    private TextView result;
+    private iCalculadora.iPresentador iPresentador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num1=(EditText)findViewById(R.id.num1);
+        num2=(EditText)findViewById(R.id.num2);
+        result = (TextView) findViewById(R.id.res);
+        iPresentador = new Presentador(this);
+    }
+
+    public void Sumar(View view){
+        Log.e("MSG: ", String.valueOf(findViewById(R.id.suma)));
+        iPresentador.calcularP(num1.getText().toString(),num2.getText().toString(),"Sumar");
+    }
+
+    @Override
+    public void mostrarRespV(String resp) {
+        result.setText(resp);
     }
 }
