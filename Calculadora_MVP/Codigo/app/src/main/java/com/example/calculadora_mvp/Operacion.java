af61e69f97f60e65f5a623852009f332535d4182
Carlos Martinez <carlos-m3@hotmail.com>
2020-06-08 00:47:35 -0500

    correccion

--- Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Operacion.java
+++ Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Operacion.java
@@ -10,12 +10,6 @@ public class Operacion {
         return new Numero(num1.getValor()-num2.getValor());
     }
 
-    public  Numero factorial(Numero num1, Numero num2){
-        Numero resultado = num1;
-        for (int i = 1; i < num1.getValor(); i++) {
-            resultado.setValor(resultado.getValor()*i);
-        }
-        return resultado;
     public Numero multiplicacion(Numero num1, Numero num2){
         return new Numero(num1.getValor()*num2.getValor());
     }
@@ -32,5 +26,13 @@ public class Operacion {
         double base=num1.getValor();
         double exponente=num2.getValor();
         return new Numero(Math.pow(base,exponente));
+    }
+
+    public  Numero factorial(Numero num1, Numero num2) {
+        double resultado =num1.getValor();
+        for (int i = 1; i < num1.getValor(); i++) {
+            resultado*=i;
+        }
+        return new Numero(resultado);
     }
 }
