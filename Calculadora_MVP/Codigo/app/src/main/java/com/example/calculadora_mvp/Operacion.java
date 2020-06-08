108f745aa9f9ba87e661c7eab6e6a61c28bd9c35
MaPaulaMV <31556567+MaPaulaMV@users.noreply.github.com>
2020-06-08 00:41:17 -0500

    operaciones parte 1

--- Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Operacion.java
+++ Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Operacion.java
@@ -1,4 +1,22 @@
 package com.example.calculadora_mvp;
 
 public class Operacion {
+
+    public Numero multiplicacion(Numero num1, Numero num2){
+        return new Numero(num1.getValor()*num2.getValor());
+    }
+
+    public Numero division(Numero num1, Numero num2){
+        try{
+            return new Numero(num1.getValor()/num2.getValor());
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public Numero exponencial(Numero num1,Numero num2){
+        double base=num1.getValor();
+        double exponente=num2.getValor();
+        return new Numero(Math.pow(base,exponente));
+    }
 }
