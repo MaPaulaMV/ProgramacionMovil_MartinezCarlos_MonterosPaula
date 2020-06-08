09b5bfd9a5c7e027cd14c1ba12010636a588ddee
Carlos Martinez <carlos-m3@hotmail.com>
2020-06-08 00:43:07 -0500

    suma, resta, factorial

--- Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Operacion.java
+++ Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Operacion.java
@@ -1,4 +1,20 @@
 package com.example.calculadora_mvp;
 
 public class Operacion {
+
+    public  Numero sumar(Numero num1, Numero num2){
+            return new Numero(num1.getValor()+num2.getValor());
+    }
+
+    public  Numero restar(Numero num1, Numero num2){
+        return new Numero(num1.getValor()-num2.getValor());
+    }
+
+    public  Numero factorial(Numero num1, Numero num2){
+        Numero resultado = num1;
+        for (int i = 1; i < num1.getValor(); i++) {
+            resultado.setValor(resultado.getValor()*i);
+        }
+        return resultado;
+    }
 }
