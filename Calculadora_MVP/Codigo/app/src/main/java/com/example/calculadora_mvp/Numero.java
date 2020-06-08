34c075f3d96f6b9b698855881cf707f1d2771bea
MaPaulaMV <31556567+MaPaulaMV@users.noreply.github.com>
2020-06-07 22:34:41 -0500

    vista parte 1 y clase numero

--- Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Numero.java
+++ Calculadora_MVP/Codigo/app/src/main/java/com/example/calculadora_mvp/Numero.java
@@ -1,4 +1,24 @@
 package com.example.calculadora_mvp;
 
 public class Numero {
+    private Double valor;
+
+    public Numero(Double valor) {
+        this.valor = valor;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero{" +
+                "valor=" + valor +
+                '}';
+    }
 }
